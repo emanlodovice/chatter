feb31019fecb6024a30317994b8da1d62aa687a9
Emmanuel Lodovice <name3anad@gmail.com>
2022-01-22 22:17:15 +0800

    Setup message recipients api endpoint

--- chat/config.py
+++ chat/config.py
@@ -1,4 +1,13 @@
+import importlib
 from django.conf import settings
 
 
 PAGE_HEADING = getattr(settings, 'CHAT_PAGE_HEADING', 'Chat')
+RECIPIENTS_METHOD_PATH = getattr(settings, 'CHAT_RECIPIENTS_METHOD', 'chat.serializers.get_users')
+module_name, get_recipients_method_name = RECIPIENTS_METHOD_PATH.rsplit('.',1)
+
+
+def get_recipients(user):
+    recipients_method_module = importlib.import_module(module_name)
+    recipients_method = getattr(recipients_method_module, get_recipients_method_name)
+    return recipients_method(user)
